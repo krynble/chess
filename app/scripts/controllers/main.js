ab8329665aaa0edb4a5ad08a3b31ee09ab8ca3c3
Omar Ajoue <krynble@gmail.com>
2014-02-01 18:50:12 -0200

    Added board to our scope

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,14 @@
 
 angular.module('chessApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.board = [
+    	[ 'BlackRook', 'BlackKnight', 'BlackBishop', 'BlackQueen', 'BlackKing', 'BlackBishop', 'BlackKnight', 'BlackRook' ],
+    	[ 'BlackPawn', 'BlackPawn', 'BlackPawn', 'BlackPawn', 'BlackPawn', 'BlackPawn', 'BlackPawn', 'BlackPawn' ],
+    	[ '', '', '', '', '', '', '', '' ],
+    	[ '', '', '', '', '', '', '', '' ],
+    	[ '', '', '', '', '', '', '', '' ],
+    	[ '', '', '', '', '', '', '', '' ],
+    	[ 'WhitePawn', 'WhitePawn', 'WhitePawn', 'WhitePawn', 'WhitePawn', 'WhitePawn', 'WhitePawn', 'WhitePawn' ],
+    	[ 'WhiteRook', 'WhiteKnight', 'WhiteBishop', 'WhiteQueen', 'WhiteKing', 'WhiteBishop', 'WhiteKnight', 'WhiteRook' ],
     ];
   });
